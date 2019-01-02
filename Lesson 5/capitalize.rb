0987c20d2753e15cb5e3cbf4c3fab9a5bbe3d9d3
EliKins <prue242004@gmail.com>
2019-01-02 17:55:45 +0200

    Added lesson 5 files

---
+++ Lesson 5/capitalize.rb
@@ -0,0 +1,5 @@
+print "Enter your name: "
+name = gets.strip
+
+puts "Your name:\t#{name.capitalize}"
+puts "Letter count:\t#{name.length}" 
---
+++ Lesson 5/hw5.rb
@@ -0,0 +1,6 @@
+begin
+	print "\r|"
+	print "\r/"
+	print "\r-"
+	print "\r\\"
+end until 1 > 2
