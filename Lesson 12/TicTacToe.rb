f60a747d644013370bdc3864f704e1726789bdc3
EliKins <prue242004@gmail.com>
2019-01-18 16:47:23 +0200

    Update TicTacToe.rb

--- Lesson 12/TicTacToe.rb
+++ Lesson 12/TicTacToe.rb
@@ -1,12 +1,107 @@
-# need to have a hash with an array (keys : 11, 12, 32, etc. like in math)
-# where there are empty spaces - have some kind of symbol
+@gameBoard = [{1 => "", 2 => "", 3 => ""}, {4 => "", 5 => "", 6 => ""}, {7 => "", 8 => "", 9 => ""}]
+@winMoves = [[1, 2, 3], [1, 5, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [4, 5, 6], [7, 8, 9]]
+@playerTurn = true
 
-# separate function: when asking for the X and Y, the key = x+y (strings)
+def emptyBoard
+	@gameBoard.each do |hash|
+		hash.each_key do |key|
+			hash[key] = :*
+			print " #{:*} "
+		end
+		puts
+	end
+end
 
-# in order to have the computer decide where to place it's symbol, create a loop which
-# randomizes 2 numbers and uses the adding function to create a key, check whether that space 
-# is empty
+def isBlockFree(move)
+	@gameBoard.each do |hash|
+		if hash[move] == :*
+			if @playerTurn
+				hash[move] = :X
+			else
+				hash[move] = :O
+			end
+			return true
+		end
+	end
+	return false
+end
 
-# find a way to create a turn-based system
+def showGameboard()
+	@gameBoard.each do |hash|
+		hash.each_value do |value|
+			print " #{value} "
+		end
+		puts
+	end
+end
 
-# loop to ask whether the user wants to play again
+def checkWin()
+  wins = 0
+	if @playerTurn
+		check = :X
+	else
+		check = :O
+	end
+
+	@winMoves.each do |arr|
+		arr.each do |x|
+			@gameBoard.each do |hash|
+				if hash.has_key?(x) && hash[x] == check
+					wins += 1
+				end
+			end
+		end
+		
+		if wins == 3
+				return true
+			end
+		wins = 0
+	end
+	return false
+end
+
+begin
+	win = false
+	emptyBoard()
+
+	if rand(0..1) == 1
+		@playerTurn = !@playerTurn
+	end
+
+	while win == false
+		if @playerTurn
+			loop do
+				print "\nYour move: "
+				move = gets.to_i
+
+				if isBlockFree(move)
+					break
+				end
+				puts "Move is invalid"
+				showGameboard()
+			end
+		else
+			print "\nComputer's move: "
+			loop do
+				num = rand(1..9)
+				if isBlockFree(num)
+					puts "#{num}"
+					break
+				end
+			end
+		end
+		showGameboard()
+
+		if checkWin()
+			if @playerTurn 
+				puts "\nPlayer wins"
+			else
+				puts "\nComputer wins"
+			end
+			break
+		end
+		@playerTurn = !@playerTurn
+	end
+	print "Do you want to play again (Y/N)? "
+	checkText = gets.strip.upcase
+end until checkText == "N"
